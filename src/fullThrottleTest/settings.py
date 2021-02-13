67e8645bb2dd479c002b2f25f08e4f5ad15fcbf6
Nitin Goyal <goyalnitin634@gmail.com>
2021-02-13 12:05:22 +0530

    :truck: restructured repository

--- src/fullThrottleTest/settings.py
+++ src/fullThrottleTest/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '8+-%m9wa2mme1@)g0#mnqsy*9bh@(l0xe8#n6psrk%w=08x7ew'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -135,4 +135,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#django_heroku.settings(locals())
+# django_heroku.settings(locals())
+STATIC_ROOT = '/vol/static'
